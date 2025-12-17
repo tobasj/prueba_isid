61df14876f2179140ce9f6930dc5c9ed8e481ddc
Cristóbal <tobajs@gmail.com>
2025-12-17 12:16:01 +0100

    SCRUM-05 comments routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
 
@@ -8,9 +9,16 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('courses', [CourseController::class, 'index']);
-    Route::post('courses', [CourseController::class, 'store']);
-    Route::get('courses/{course}', [CourseController::class, 'show']);
-    Route::put('courses/{course}', [CourseController::class, 'update']);
-    Route::delete('courses/{course}', [CourseController::class, 'destroy']);
+    Route::prefix('courses')->group(function () {
+        Route::get('courses', [CourseController::class, 'index']);
+        Route::post('courses', [CourseController::class, 'store']);
+        Route::get('courses/{course}', [CourseController::class, 'show']);
+        Route::put('courses/{course}', [CourseController::class, 'update']);
+        Route::delete('courses/{course}', [CourseController::class, 'destroy']);
+
+        Route::prefix('{course}/comments')->group(function () {
+            Route::get('/', [CommentController::class, 'index']);
+            Route::post('/', [CommentController::class, 'store']);
+        });
+    });
 });
