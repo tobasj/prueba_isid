5bbcd1f70f76b8ac9e8ea2b4d3ed6176f8871890
Cristóbal <tobajs@gmail.com>
2025-12-17 11:20:21 +0100

    SCRUM-04 courses routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('courses', [CourseController::class, 'index']);
+    Route::post('courses', [CourseController::class, 'store']);
+    //Route::get('courses/{course}', [CourseController::class, 'show']);
+    //Route::put('courses/{course}', [CourseController::class, 'update']);
+    Route::delete('courses/{course}', [CourseController::class, 'destroy']);
+});
